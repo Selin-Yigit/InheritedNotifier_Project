5ecd28e5455ef9e2487c2f823d05a129f5fc07aa
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-02 23:03:32 +0300

    HomePage tamamlandı.

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:inheritednotifier_project/sliderData.dart';
+import 'package:inheritednotifier_project/sliderIheritedNotifier.dart';
+
+final sliderdata = SliderData();
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  
+  HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,29 +21,45 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Slider(
-            thumbColor: Colors.deepPurple.shade300,
-            activeColor: Colors.deepPurple.shade300,
-            inactiveColor: const Color.fromARGB(150, 149, 117, 205),
-            value: 0.0,
-            onChanged: (value) {},
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
+      body: SliderInheritedNotifier(
+        sliderdata: sliderdata,
+        child: Builder(builder: (context) {
+          // Burada builder kullanılmasının sebebi, sliderın değerini "SliderInheritedNotifier" 'dan almalıyız.
+          //Fakat "SliderInheritedNotifier" 'da tanımladığımız of fonksiyonu içinde kullandığımız context,
+          // HomePage'e ait contextten sonra oluşturulduğu için "0.0" geliyo bu sebeple slider hareket etmiyor.
+          return Column(
             children: [
-              Container(
-                color: Colors.pink.shade200,
-                height: 200,
+              Slider(
+                thumbColor: Colors.deepPurple.shade300,
+                activeColor: Colors.deepPurple.shade300,
+                inactiveColor: const Color.fromARGB(150, 149, 117, 205),
+                value: SliderInheritedNotifier.of(context),
+                onChanged: (value) {
+                  sliderdata.value = value;
+                },
               ),
-              Container(
-                color: Colors.blue.shade200,
-                height: 200,
-              ),
-            ].expandEqually().toList(),
-          )
-        ],
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Opacity(
+                    opacity: SliderInheritedNotifier.of(context),
+                    child: Container(
+                      color: Colors.pink.shade200,
+                      height: 200,
+                    ),
+                  ),
+                  Opacity(
+                    opacity: SliderInheritedNotifier.of(context),
+                    child: Container(
+                      color: Colors.blue.shade200,
+                      height: 200,
+                    ),
+                  ),
+                ].expandEqually().toList(),
+              )
+            ],
+          );
+        }),
       ),
     );
   }
