a4eaedfbb0bedabbd25b270dbd355b57fc47da25
Selin Yigit <sln.ygt.41@gmail.com>
2023-11-02 22:35:23 +0300

    HomePage ve Slider Data oluşturuldu.

---
+++ lib/homePage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple.shade300,
+        centerTitle: true,
+        title: const Text(
+          "Inherited Notifier Project",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Slider(
+            thumbColor: Colors.deepPurple.shade300,
+            activeColor: Colors.deepPurple.shade300,
+            inactiveColor: const Color.fromARGB(150, 149, 117, 205),
+            value: 0.0,
+            onChanged: (value) {},
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                color: Colors.pink.shade200,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue.shade200,
+                height: 200,
+              ),
+            ].expandEqually().toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (w) => Expanded(child: w),
+      );
+}
---
+++ lib/sliderData.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+
+  double get value => _value;
+  set value(double newValue) {
+    if (newValue != _value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
